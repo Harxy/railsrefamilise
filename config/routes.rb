06f5826bebf46d4664df1e4bd42e21f9aa27313e
Harxy <harkerp@gmail.com>
2016-06-29 23:02:10 +0100

    added notes as the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'notes#index'
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
   get '/auth/logout' => "auth0#logout"
