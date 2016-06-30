a04d5bbb69334074b87f39a7a62f385c65a76159
Harxy <harkerp@gmail.com>
2016-06-30 10:21:55 +0100

    root removed - causes page errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  root 'notes#index'
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
   get '/auth/logout' => "auth0#logout"
